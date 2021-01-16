89a1050d364bbd3eafbe56d4c5e3093a12b6bb7a
Iwo Herka <iwo.herka@makimo.pl>
2021-01-16 09:25:45 +0100

    Second commit

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
 
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.json "1.0.0"]
                  [clj-http "3.10.3"]]
 
   :main ^:skip-aot cat-fact-generator.core
--- src/cat_fact_generator/delivery/api.clj
+++ src/cat_fact_generator/delivery/api.clj
@@ -1,7 +1,12 @@
 (ns cat-fact-generator.delivery.api
   (:require [cat-fact-generator.core.entity :as e]
+            [clojure.data.json :as json]
             [clj-http.client :as http]))
 
 (defrecord ApiProvider []
   e/FactProvider
   (-get-fact [this] ))
+
+(json/read-str (:body (http/get "https://catfact.ninja/fact")))
+
+(get (http/get "https://catfact.ninja/fact") :body)
